210959615eea576c6fff797546138c086243bfe4
Ernad Husremovic <hernad@bring.out.ba>
2021-01-12 13:49:25 +0100

    mora postojati extension.js

--- scripts/publish_openvsx.sh
+++ scripts/publish_openvsx.sh
@@ -2,6 +2,13 @@
 
 echo PREREQ: "yarn install"
 
+if  [[ ! -f out/extension.js ]]
+then
+   echo "gdje je extension.js?"
+   exit 100
+fi
+
+
 echo publish to openvsx ...
 OPENVSX_TOKEN=`cat $HOME/.openvsx_token`
 echo "OPENVSX_TOKEN=$OPENVSX_TOKEN"
